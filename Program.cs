0fccd60bb418a7d61b83d0ac4457673b11c041a3
ogagaoghene emmanuel <esavwede84@gmail.com>
2024-01-06 12:11:49 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+// Single Line comment 
+/* Multiline Comment */
+
+namespace mainNamespace
+{
+    class MainClass 
+    {       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I love programming in C# "); 
+            Console.WriteLine("What is your name ? ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Welcome to C# " + name  + " how old are you ?"); 
+            int age = Convert.ToInt16( Console.ReadLine() );
+            Console.WriteLine( name + " is " + age + " years old ");
+        }
+    }
+}
